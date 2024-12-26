36ad8a27b32329741e65a6eb8a69f7b92e046dc0
luyingquan <996760697@qq.com>
2024-12-26 13:15:50 +0800

    fix: update resource paths to relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ const __dirname = dirname(__filename)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/trade-management-systempublic/',
+  base: './',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
